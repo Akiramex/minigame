2533219690f7f3ca9e3bd597620678ee101d7762
Akira <1113139776@qq.com>
2024-07-30 14:04:37 +0800

    7/30

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,13 @@ mod utils;
 mod monster;
 
 pub use crate::monster::Monster; 
-use std::{io, thread, process, time::Duration};
+use std::{collections::VecDeque, io, process, thread, time::Duration};
+use monster::State;
 use rand::Rng;
 use colored::*;
 fn start() {
 
-    let mut has_monster = Vec::<Monster>::new();
+    let mut has_monster = VecDeque::<Monster>::new();
 
     println!("{}", "Welcome to minigame".blue());
 
@@ -31,7 +32,7 @@ fn start() {
         match choise {
             1 => {
                 println!("{}", "--------------------------".purple());
-                has_monster.push(create_monster());
+                has_monster.push_back(create_monster());
             },
             2 => {
                 println!("{}", "--------------------------".purple());
@@ -70,7 +71,7 @@ fn create_monster() -> Monster {
     new_monster
 }
 
-fn show_monster(list: &[Monster]) {
+fn show_monster(list: &VecDeque<Monster>) {
     for (i, item) in list.iter().enumerate() {
         println!("No.{i} monster");
         println!("{item}\n")
@@ -99,93 +100,151 @@ fn get_random_monster(begin :u32, end :u32) -> Result<Monster, &'static str>{
 
     let base_name= vec!["Yoto", "Leo", "Akira", "Mila", "Santo", "Alan"];
 
-    let rand_name_index = rand::thread_rng().gen_range(0..=base_name.len());
+    let rand_name_index = rand::thread_rng().gen_range(0..base_name.len());
     let rand_hp  = rand::thread_rng().gen_range(begin..=end) as i32;
     let rand_mp = rand::thread_rng().gen_range(begin..=end) as i32;
     let rand_attack = rand::thread_rng().gen_range(0..=rand_attack_end) as i32;
 
     let rand_name = base_name[rand_name_index];
 
-
+  
     Ok(Monster::build(rand_name.to_string(), rand_hp, rand_mp, rand_attack))
 }
 
-fn go_outside(list: &mut Vec<Monster>) {
+fn go_outside(list: &mut VecDeque<Monster>) {
 
     if list.is_empty() {
         println!("你还没有Monster呢，去创建你的Monster吧!");
         return;
     }
 
-    println!("{}", "在一脚踢飞路边的野草!!".green());
+    println!("{}", "一脚踢飞路边的野草!!".green());
 
-    let mut rand_monster  =  get_random_monster(0, 50).unwrap();
+    let mut rand_monster  =  get_random_monster(1, 50).unwrap();
     thread::sleep(Duration::from_secs(1));
     println!("{}{}", "遭遇了野生的Monster: ".red(), rand_monster.name());
-
-    loop {
-        show_battlle();
-        println!("{}", "--------------------------".purple());
-
-        let mut buffer = String::new();
     
-        io::stdin().read_line(&mut buffer).expect("Failed to read line");
 
-        let choise : i32 = match buffer.trim().parse() {
-            Ok(num) => num,
-            Err(_) => {
-                println!("Error input");
-                continue
-            },
-        };
 
-        match choise {
-            1 => {
-                if let Some(monster) = list.get(0) {
-                    let (a, h) = monster.normal_attack(&mut rand_monster);
-                    println!("你的Monster:{}，使用了普通攻击，对{}造成了{}点伤害， {}剩下{}血", monster.name(), rand_monster.name(), a, rand_monster.name(), h);
-                    thread::sleep(Duration::from_secs(1));
-                }
-            },
-            2 => {
-                if let Some(monster) = list.get(0) {
-                    println!("你的Monster:{}，使用了重击，但是摔倒了，Miss~", monster.name());
-                    thread::sleep(Duration::from_secs(1));
-                }
-            },
-            3 => {
-                if let Some(monster) = list.get(0) {
-                    println!("你的Monster:{} 在发呆~", monster.name());
-                    thread::sleep(Duration::from_secs(1));
-                }
-            },
-            _ => (),
+    'bigloop: loop {
+
+        let monster_index;
+
+        // 每次循环检测一下monster的状态
+        let mut alive_monster_num = 0;
+        for monster in list.iter() {
+            if let State::Live = monster.state() {
+                alive_monster_num += 1;
+            }
         }
-        
 
-        if rand_monster.hp() < 0 {
-            println!("登登登，战胜了{}，获得了xp", rand_monster.name());
-            thread::sleep(Duration::from_secs(2));
-            println!("{}", "YOU WIN".red());
+        if alive_monster_num == 0 {
+            println!("{}", "你的Monster全睡着了".red());
+            println!("{}", "GAME OVER".red());
             break;
         }
-        
+        else {
+            println!("选择你出战的Monster：");
+            show_monster(list);
 
-        if let Some(m) = list.get_mut(0) {
-            let mut monster = m;
+            loop {
+                let mut buffer = String::new();
+                io::stdin().read_line(&mut buffer).expect("Failed to read line");
+                let choise : usize = match buffer.trim().parse() {
+                    Ok(num) => num,
+                    Err(_) => {
+                        println!("Error input");
+                        continue
+                    },
+                };
+
+                if let Some(m) = list.get_mut(choise) {
+                    match m.state() {
+                        State::Live => {
+                            monster_index = choise;
+                            break;
+                        }
+                        State::Sleep => {
+                            println!("{}睡着了，不能塔塔开", m.name());
+                            continue
+                        },
+                    };
+                } else {
+                    continue;
+                }
+            }
+        }
+
+
+        loop {
+            // 选择战斗指令
+            // 我方睡着break
+            // 敌方睡着直接break 'bigloop;
+
+            show_battlle();
+            println!("{}", "--------------------------".purple());
+    
+            let mut buffer = String::new();
+        
+            io::stdin().read_line(&mut buffer).expect("Failed to read line");
+    
+            let choise : i32 = match buffer.trim().parse() {
+                Ok(num) => num,
+                Err(_) => {
+                    println!("Error input");
+                    continue
+                },
+            };
+    
+            match choise {
+                1 => {
+                    if let Some(mst) = list.get_mut(monster_index) {
+                        let (a, h) = mst.normal_attack(&mut rand_monster);
+                        println!("你的Monster:{}，使用了普通攻击，对{}造成了{}点伤害， {}剩下{}血", mst.name(), rand_monster.name(), a, rand_monster.name(), h);
+                        thread::sleep(Duration::from_secs(1));
+                    }
+                },
+                2 => {
+                    if let Some(mst) = list.get_mut(monster_index) {
+                        println!("你的Monster:{}，使用了重击，但是摔倒了，Miss~", mst.name());
+                        thread::sleep(Duration::from_secs(1));
+                    }
+                },
+                3 => {
+                    if let Some(mst) = list.get_mut(monster_index) {
+                        println!("你的Monster:{} 在发呆~", mst.name());
+                        thread::sleep(Duration::from_secs(1));
+                    }
+                },
+                _ => (),
+            }
             
+    
+            if let State::Sleep = rand_monster.state() {
+                println!("登登登，战胜了{}，获得了xp", rand_monster.name());
+
+                println!("{}", "YOU WIN".red());
+                thread::sleep(Duration::from_secs(2));
+                break 'bigloop;
+            }
+            
+                
             let rand_index  = rand::thread_rng().gen_range(1..=10);
             // 这里搞怪物的逻辑
             match rand_index {
                 1..=6 => {
-                    let (a, h) = rand_monster.normal_attack(&mut monster);
-                    println!("Monster:{}，使用了普通攻击，对{}造成了{}点伤害， {}剩下{}血", rand_monster.name(), monster.name(), a, monster.name(), h);
-                    thread::sleep(Duration::from_secs(1));
+                    if let Some(mst) = list.get_mut(monster_index) {
+                        let (a, h) = rand_monster.normal_attack(mst);
+                        println!("Monster:{}，使用了普通攻击，对{}造成了{}点伤害， {}剩下{}血", rand_monster.name(), mst.name(), a, mst.name(), h);
+                        thread::sleep(Duration::from_secs(1));
+                    }
                 },
                 7|8 => {
-                    let (a, h) = rand_monster.power_attack(&mut monster);
-                    println!("Monster:{}，使用了重击，对{}造成了{}点伤害， {}剩下{}血", rand_monster.name(), monster.name(), a, monster.name(), h);
-                    thread::sleep(Duration::from_secs(1));
+                    if let Some(mst) = list.get_mut(monster_index) {
+                        let (a, h) = rand_monster.power_attack(mst);
+                        println!("Monster:{}，使用了重击，对{}造成了{}点伤害， {}剩下{}血", rand_monster.name(), mst.name(), a, mst.name(), h);
+                        thread::sleep(Duration::from_secs(1));
+                    }
                 },
                 9|10 => {
                     println!("Monster:{}正在观察", rand_monster.name());
@@ -194,13 +253,12 @@ fn go_outside(list: &mut Vec<Monster>) {
                 _ => (),
             }
     
-
-            if monster.hp() < 0 {
-                println!("你的Monster{},由于血量低于0, 睡着了~", monster.name());
-                thread::sleep(Duration::from_secs(2));
-                println!("{}", "GAME OVER".red());
-
-                break;
+            if let Some(mst) = list.get(monster_index) {
+                if let State::Sleep = mst.state() {
+                    println!("你的Monster:{},由于血量低于0, 睡着了~", mst.name());
+                    thread::sleep(Duration::from_secs(2));
+                    break;
+                }
             }
         }
     }
@@ -235,10 +293,10 @@ mod tests {
 
     #[test]
     fn show_test() {
-        let mut has_monster = Vec::<Monster>::new();
+        let mut has_monster = VecDeque::<Monster>::new();
 
-        has_monster.push(Monster::new());
-        has_monster.push(Monster::new());
+        has_monster.push_back(Monster::new());
+        has_monster.push_back(Monster::new());
 
         show_monster(&has_monster);
 
--- src/monster.rs
+++ src/monster.rs
@@ -1,15 +1,37 @@
 use::std::fmt;
 
+#[derive(Clone)]
+pub enum State {
+    Live,
+    Sleep,
+}
+
+impl fmt::Display for State {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Live => write!(f, "存活"),
+            Self::Sleep => write!(f, "睡着"),
+        }
+    }
+}
+
 pub struct Monster {
     name: String,
     hp: i32,
+    max_hp: i32,
     mp: i32,
+    max_mp: i32,
     attack: i32,
+    state: State,
 }
 
 impl fmt::Display for Monster {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "Name: {}\nHP: {}\nMP: {}\nattack: {}", self.name, self.hp, self.mp, self.attack)
+        writeln!(f, "Name: {}", self.name)?;
+        writeln!(f, "HP: {}/{}", self.hp, self.max_hp)?;
+        writeln!(f, "MP: {}/{}", self.mp, self.max_mp)?;
+        writeln!(f, "attack: {}", self.attack)?;
+        write!(f, "State: {}",self.state)
     }
 }
 
@@ -17,18 +39,36 @@ impl Monster {
     pub fn new() -> Monster {
         Monster {
             name : String::from("base_monster"),
+            max_hp: 100,
+            max_mp: 100,
             hp: 100,
             mp: 100,
             attack: 10,
+            state: State::Live,
         }
     }
 
     pub fn build(name: String, hp: i32, mp: i32, attack: i32) -> Monster {
         Monster {
             name,
+            max_hp: hp,
+            max_mp: mp,
             hp,
             mp,
-            attack
+            attack,
+            state: State::Live,
+        }
+    }
+
+    pub fn build_from(monster: &Monster) -> Monster {
+        Monster {
+            name: monster.name.clone(),
+            state: monster.state.clone(),
+            hp: monster.hp,
+            mp: monster.mp,
+            max_hp: monster.hp,
+            max_mp: monster.mp,
+            attack: monster.attack,
         }
     }
 
@@ -48,13 +88,27 @@ impl Monster {
         &self.name
     }
 
+    pub fn state(&self) -> &State {
+        &self.state
+    }
+
     pub fn normal_attack(&self, target: &mut Self) -> (i32, i32) {
         target.hp -= self.attack;
+
+        if target.hp <= 0 {
+            target.state = State::Sleep;
+        }
+
         (self.attack, target.hp)
     }
 
     pub fn power_attack(&self, target: &mut Self) -> (i32, i32) {
         target.hp -= self.attack*2;
-        (self.attack, target.hp)
+
+        if target.hp <= 0 {
+            target.state = State::Sleep;
+        }
+
+        (self.attack*2, target.hp)
     }
 }
